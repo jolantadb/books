9bfac9e6a1ccf16cf7a4d0249384f73495412b45
Jolanta Baranovska <jolantadbaranovska@gmail.com>
2021-12-19 12:05:52 +0200

    Book.txt file read

--- src/main/java/lv/sda/books/Bookstore.java
+++ src/main/java/lv/sda/books/Bookstore.java
@@ -1,13 +1,44 @@
 package lv.sda.books;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
 
 public class Bookstore {
     // saraksts ar grāmatam
+    List<Book >books = new ArrayList<>();
 
+    public Bookstore() {
+        try {
+            Path path = Paths.get("src/main/resources/books.txt");
+            List<Book> books = Files.lines(path)
+                    .map(line -> {
+                        List<String> fields = Arrays.stream(line.split(";")).collect(toList());
+                        return new Book(
+                                fields.get(0),
+                                fields.get(1),
+                                fields.get(2),
+                                fields.get(3),
+                                fields.get(4),
+                                Integer.parseInt(fields.get(5)),
+                                LocalDate.of(Integer.parseInt(fields.get(6)), 1, 1)
+                        );
+                    })
+                    .collect(toList());
+            this.books.addAll(books);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void addBook (Book book){
 //        pievieno grāmatu
